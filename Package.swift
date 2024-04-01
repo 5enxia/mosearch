24f9ac33e0bacad6730bebcdf866f2f7459f682c
5enxia <takayasumorishita@gmail.com>
2024-04-02 01:42:58 +0900

    Add sqlite package

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Mosearch",
+    dependencies: [
+        .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.15.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
