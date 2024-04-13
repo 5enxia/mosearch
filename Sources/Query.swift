bdd5eec3c60307c1d510c052ceaa8d9eb3958414
5enxia <takayasumorishita@gmail.com>
2024-04-13 18:45:22 +0900

    Add init func to Query struct

--- Sources/Query.swift
+++ Sources/Query.swift
@@ -4,6 +4,13 @@ public struct MatchQuery {
     var analyzer: Analyzer
     var sorter: Sorter
 
+    public init(keyword: String, logic: Logic, analyzer: Analyzer, sorter: Sorter) {
+        self.keyword = keyword
+        self.logic = logic
+        self.analyzer = analyzer
+        self.sorter = sorter
+    }
+
     public func searcher(storage: Storage) -> Searcher {
         let tokenStream = self.analyzer.analyze(self.keyword)
         return MatchSearcher(tokenStream: tokenStream, logic: self.logic, storage: storage, sorter: self.sorter)
