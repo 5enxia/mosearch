b9b0a10e8271e70dfe80572eb5df56dbe4c84d54
5enxia <takayasumorishita@gmail.com>
2024-04-13 18:45:04 +0900

    Add init func to Document struct

--- Sources/Document.swift
+++ Sources/Document.swift
@@ -6,4 +6,10 @@ public struct Document {
     var id: DocumentID
     var body: String
     var tokenCount: Int
+
+    public init(id: DocumentID = 0, body: String = "", tokenCount: Int = 0) {
+        self.id = id
+        self.body = body
+        self.tokenCount = tokenCount
+    }
 }
